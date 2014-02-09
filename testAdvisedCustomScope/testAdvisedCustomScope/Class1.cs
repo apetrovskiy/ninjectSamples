454df51bf5aaadbb311709d5fce8068891c1ccea
Alexander Petrovskiy <uiautomation@hotmail.com>
2014-02-10 01:25:58 +0400

    .

---
+++ testAdvisedCustomScope/testAdvisedCustomScope/Class1.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 1/29/2014
+ * Time: 12:03 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testAdvisedCustomScope
+{
+    /// <summary>
+    /// Description of Class1.
+    /// </summary>
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
