8c41fa2dd47d1d6065092aa0d422f3ca1f1ec386
apetrovskiy <apetrovskiy@hotmail.com>
2015-04-28 15:50:09 +0300

    .

---
+++ NinjectInterception/NinjectInterception/MyModule.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 4/28/2015
+ * Time: 1:35 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace NinjectInterception
+{
+    using System;
+    using Ninject.Modules;
+    using Ninject.Extensions.Interception.Infrastructure.Language;
+    
+    /// <summary>
+    /// Description of Module.
+    /// </summary>
+    public class MyModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<MyClass>().ToSelf().Intercept().With<ILoggerAspect>();
+        }
+    }
+}
